e43d38ca80bedd8615c4166340bf6eca97db4ec7
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-07-08 22:54:36 -0500

    Fixed pagination

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,97 +1,113 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';  
+// import CardCount from './Footer/CardCount.js';
+// import Pagination from './Footer/Pagination.js';
+
+// const Footer = ({ cards, setPageNumber, pageNumber, numberOfPages }) => {
+//   const firstCard = pageNumber * 12 + 1;
+//   const lastCard = Math.min((pageNumber + 1) * 12, cards.length * numberOfPages); 
+//   const [cardCount, setCardCount] = useState(null);
+
+//   useEffect(() => {
+//     const fetchCardCount = async () => {
+//       try {
+//         const response = await axios.get('/api/cards/count'); // Adjust URL as per your backend setup
+//         setCardCount(response.data.totalCards);
+//       } catch (error) {
+//         console.error('Error fetching card count:', error);
+//       }
+//     };
+
+//     fetchCardCount();
+//   }, []);
+
+//   const gotoPrevious = () => {
+//     setPageNumber(Math.max(0, pageNumber - 1));
+//   };
+
+//   const gotoNext = () => {
+//     setPageNumber(Math.min(numberOfPages - 1, pageNumber + 1));
+//   };
+
+//   const onPageChange = (pageIndex) => {
+//     setPageNumber(pageIndex);
+//   };
+
+//   return (
+//     <div>   
+//       {/* Render Pagination */}
+//       <Pagination
+//         currentPage={pageNumber}
+//         totalPages={numberOfPages}
+//         onPageChange={onPageChange}
+//         gotoPrevious={gotoPrevious}
+//         gotoNext={gotoNext}
+//       />
+
+//       {/* Desktop Layout */}
+//       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+//         <CardCount firstCard={firstCard} lastCard={lastCard} cardCount={cardCount} />   
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+
+
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';  
 import CardCount from './Footer/CardCount.js';
+import Pagination from './Footer/Pagination.js';
 
-const Footer = ({ cards, setPageNumber, pageNumber, numberOfPages, pages }) => {
+const Footer = ({ cards, setPageNumber, pageNumber, numberOfPages }) => {
   let firstCard = pageNumber * 12 + 1;
   let lastCard = Math.min((pageNumber + 1) * 12, cards.length * numberOfPages); 
   const [cardCount, setCardCount] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  console.log(cards);
+  useEffect(() => {
+    const fetchCardCount = async () => {
+      try {
+        const response = await axios.get('/api/cards/count'); 
+        setCardCount(response.data.totalCards);
+      } catch (error) {
+        console.error('Error fetching card count:', error);
+      }
+    };
 
+    fetchCardCount();
+  }, []);
+  
   const gotoPrevious = () => {
     setPageNumber(Math.max(0, pageNumber - 1));
+    console.log(pageNumber);
   };
 
   const gotoNext = () => {
     setPageNumber(Math.min(numberOfPages - 1, pageNumber + 1));
   };
 
-  useEffect(() => {
-    const fetchCardCount = async () => {
-      try {
-        const response = await axios.get('/api/cards/count'); // Adjust URL as per your backend setup
-        setCardCount(response.data.totalCards);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching card count:', error);
-        setError('Error fetching card count. Please try again later.');
-        setLoading(false);
-      }
-    };
-
-    fetchCardCount();
-  }, []);
+  const onPageChange = (pageIndex) => {
+    setPageNumber(pageIndex);
+  };
 
   return (
     <div>   
-        {pages.map((pageIndex) => (
-          <button key={pageIndex} onClick={() => setPageNumber(pageIndex)}>
-            {pageIndex + 1}
-          </button>
-        ))}
-
-      {/* Desktop Layout */}
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <CardCount firstCard={firstCard} lastCard={lastCard} cardCount={cardCount} />   
       </div>
 
-      {/* Tailwind Styled */}
-      {/* Main Pagination */}
-      <div>   
-        <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-          {/* Left Arrow Button */}
-          <a href="#" onClick={gotoPrevious} className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-            <span className="sr-only">Previous</span>
-            <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
-            </svg>
-          </a>
-
-
-          {/* Replace with dynamic page numbers based on your logic */}
-          {/* 1 */}
-          <a href="#" aria-current="page" className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-          
-          {/* 2 */}
-          <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
-          
-          {/* 3 */}
-          <a href="#" className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
-          
-          {/* Span button */}
-          <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span>
-          
-          {/* 8 */}
-          <a href="#" className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">8</a>
-          
-          {/* 9 */}
-          <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">9</a>
-          
-          {/* Right Arrow Button */}
-          <a href="#" onClick={gotoNext} className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-            <span className="sr-only">Next</span>
-            <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-            </svg>
-          </a>
-        </nav>
-      </div>
+      < Pagination 
+        currentPage={pageNumber} 
+        totalPages={numberOfPages} 
+        onPageChange={onPageChange} 
+        gotoPrevious={gotoPrevious} 
+        gotoNext={gotoNext} 
+      />
 
     </div>
-  )
+  );
 };
 
 export default Footer;
